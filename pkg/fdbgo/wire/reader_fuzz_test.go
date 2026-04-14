e05e5dfb9079b98276e63005d8e6652f4b19d113
Johannes Brüderl <johannes.bruederl@gmail.com>
2026-04-14 19:11:36 +0200

    test: add fuzz targets for wire Reader constructor + ErrorOr
    FuzzNewReader: constructs Reader from crafted data, exercises all
    field access methods (ReadInt32/64, ReadBytes, ReadString, ReadBool,
    ReadUID, etc.) on 10 slots. Verifies no panics.
    
    FuzzReadErrorOrInto: fuzzes the ErrorOr wrapper used by all reply
    parsers.
    
    Both pass (60s each). Total fuzz targets: 21.

---
+++ pkg/fdbgo/wire/reader_fuzz_test.go
@@ -0,0 +1,45 @@
+package wire
+
+// Fuzz the wire Reader constructor and field access methods.
+// NewReader parses FlatBuffers headers — crafted data must not panic.
+
+import "testing"
+
+func FuzzNewReader(f *testing.F) {
+	f.Add([]byte{})
+	f.Add([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+	f.Add(make([]byte, 100))
+	f.Add([]byte{0x0F, 0xDB, 0x00, 0xB0, 0x73, 0x00, 0x00, 0x00, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		r, err := NewReader(data)
+		if err != nil {
+			return
+		}
+		// Exercise field access methods — none should panic.
+		_ = r.VTableLength()
+		for i := 0; i < 10; i++ {
+			_ = r.FieldPresent(i)
+			_ = r.ReadInt32(i)
+			_ = r.ReadUint32(i)
+			_ = r.ReadInt64(i)
+			_ = r.ReadUint64(i)
+			_ = r.ReadFloat64(i)
+			_ = r.ReadBool(i)
+			_ = r.ReadBytes(i)
+			_ = r.ReadString(i)
+			_ = r.ReadUID(i)
+		}
+	})
+}
+
+func FuzzReadErrorOrInto(f *testing.F) {
+	f.Add([]byte{})
+	f.Add([]byte{0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00})
+	f.Add(make([]byte, 50))
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		var r Reader
+		_ = ReadErrorOrInto(data, &r)
+	})
+}
