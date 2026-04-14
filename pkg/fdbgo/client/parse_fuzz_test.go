ef958086e61908336c71505586ba50ab9976fe68
Johannes Brüderl <johannes.bruederl@gmail.com>
2026-04-14 17:01:09 +0200

    test: add fuzz targets for wire protocol reply parsers
    4 new fuzz targets for parseGetKeyReply, parseGetValueReply,
    parseGetKeyValuesReply, parseGetReadVersionReply. Verify that
    malformed wire responses don't cause panics. All pass (30s each,
    no crashes found).

---
+++ pkg/fdbgo/client/parse_fuzz_test.go
@@ -0,0 +1,53 @@
+package client
+
+// Fuzz targets for wire protocol reply parsers.
+// These verify that no combination of input bytes causes a panic.
+// A production FDB server should never send garbage, but network corruption
+// or a misbehaving proxy must not crash the client.
+
+import "testing"
+
+func FuzzParseGetKeyReply(f *testing.F) {
+	f.Add([]byte{})
+	f.Add([]byte{0x00})
+	f.Add([]byte{0xFF, 0xFF, 0xFF, 0xFF})
+	f.Add(make([]byte, 100))
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		// Must not panic. Errors are fine.
+		parseGetKeyReply(data)
+	})
+}
+
+func FuzzParseGetValueReply(f *testing.F) {
+	f.Add([]byte{})
+	f.Add([]byte{0x00})
+	f.Add([]byte{0xFF, 0xFF, 0xFF, 0xFF})
+	f.Add(make([]byte, 100))
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		parseGetValueReply(data)
+	})
+}
+
+func FuzzParseGetKeyValuesReply(f *testing.F) {
+	f.Add([]byte{})
+	f.Add([]byte{0x00})
+	f.Add([]byte{0xFF, 0xFF, 0xFF, 0xFF})
+	f.Add(make([]byte, 200))
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		parseGetKeyValuesReply(data)
+	})
+}
+
+func FuzzParseGetReadVersionReply(f *testing.F) {
+	f.Add([]byte{})
+	f.Add([]byte{0x00})
+	f.Add([]byte{0xFF, 0xFF, 0xFF, 0xFF})
+	f.Add(make([]byte, 100))
+
+	f.Fuzz(func(t *testing.T, data []byte) {
+		parseGetReadVersionReply(data)
+	})
+}
